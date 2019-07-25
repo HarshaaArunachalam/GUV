81b374fb0b5421e23a60591a7e9c9c47866fc09f
HarshaaArunachalam <51322955+HarshaaArunachalam@users.noreply.github.com>
2019-07-25 23:56:42 +0530

    Update 15.py

--- pro/15.py
+++ pro/15.py
@@ -1,6 +1,6 @@
-u=int(input())
-v=list(map(int,input().split()))
-w=[]
-for i in range(u):
-    v.sort(reverse=True)
-print(*v,sep="\n")
+x=int(input())
+y=list(map(int,input().split()))
+z=[]
+for i in range(x):
+    y.sort(reverse=True)
+print(*y,sep="\n")
