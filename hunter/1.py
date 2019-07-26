c2144f876d5fdd1ea3313347c5b7b51989a2723d
HarshaaArunachalam <51322955+HarshaaArunachalam@users.noreply.github.com>
2019-07-27 00:13:51 +0530

    Create 1.py

---
+++ hunter/1.py
@@ -0,0 +1,13 @@
+n=int(input())
+lists=input().split()
+lists=list(lists)
+def Repeat(x): 
+    _size = len(x) 
+    repeated = [] 
+    for i in range(_size): 
+        k = i + 1
+        for j in range(k, _size): 
+            if x[i] == x[j] and x[i] not in repeated: 
+                repeated.append(x[i]) 
+    return sorted(repeated) 
+print(*Repeat(lists))
